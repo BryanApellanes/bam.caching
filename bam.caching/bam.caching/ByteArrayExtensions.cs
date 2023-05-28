d0cc5d8de17d9d6761c37afc71755109783a29de
Bryan Apellanes <bryan@threeheadz.com>
2023-05-27 21:15:20 -0500

    it compiles

---
+++ bam.caching/bam.caching/ByteArrayExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bam.Caching
+{
+    public static class ByteArrayExtensions
+    {
+        public static Task<byte[]> GZipAsync(this byte[] data)
+        {
+            return Task.Run(() => data.GZip());
+        }
+
+        public static byte[] GZip(this byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (GZipStream zipStream = new GZipStream(ms, CompressionMode.Compress, true))
+                {
+                    zipStream.Write(data, 0, data.Length);
+                }
+
+                data = ms.ToArray();
+            }
+
+            return data;
+        }
+
+    }
+}
---
+++ bam.caching/bam.caching/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bam.Caching;
+
+namespace Bam.Net.Caching
+{
+    public static class StringExtensions
+    {
+        public static Task<byte[]> GZipAsync(this string value, Encoding enc = null)
+        {
+            return Task.Run(() => value.GZip(enc));
+        }
+
+        public static byte[] GZip(this string value, Encoding enc = null)
+        {
+            enc = enc ?? Encoding.UTF8;
+            return enc.GetBytes(value).GZip();
+        }
+    }
+}
